c17d03a911ce97232c04824592e9737a31dfe61a
Samuel Faustino <104699456+SamuelFaustino@users.noreply.github.com>
2024-06-06 23:01:08 -0300

    Delete rest directory

--- rest/src/main/java/br/edu/uniara/lpi2/rest/model/EmployeePagingRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.uniara.lpi2.rest.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface EmployeePagingRepository
-        extends PagingAndSortingRepository<Employee, Long> {
-}
--- rest/src/main/java/br/edu/uniara/lpi2/rest/model/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.uniara.lpi2.rest.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
